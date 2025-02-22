230b0e9f8f4efbf275a3d9200693e1ff7436ff6b
wo-o29 <woogur29@gmail.com>
2025-02-22 13:18:34 +0900

    feat: 로또 구입 로직 추가

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,4 +1,33 @@
-/**
- * step 2의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import { LOTTO_RULE } from "./constants/lotto.js";
+import generateLottoNumberSets from "./lotto/generateLottoNumberSets.js";
+import { isInRange } from "./utils/predicate.js";
+import validationLottoPrice from "./validation/validationLottoPrice.js";
+
+const $buyLottoForm = document.querySelector(".buy-lotto-form");
+const $buyLottoInput = document.querySelector(".buy-lotto-input");
+const $buyLottoSubmitButton = document.querySelector(
+  ".buy-lotto-submit-button"
+);
+
+$buyLottoInput.addEventListener("input", () => {
+  const price = Number($buyLottoInput.value);
+
+  if (isInRange(price, LOTTO_RULE.MIN_PRICE, LOTTO_RULE.MAX_PRICE)) {
+    $buyLottoSubmitButton.disabled = false;
+    return;
+  }
+
+  $buyLottoSubmitButton.disabled = true;
+});
+
+$buyLottoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  try {
+    const lottoPrice = Number($buyLottoInput.value);
+    validationLottoPrice(lottoPrice);
+    const lottoNumberSets = generateLottoNumberSets(lottoPrice);
+  } catch (error) {
+    alert(error.message);
+  }
+});
