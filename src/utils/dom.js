65929e2745aa94615a037b3d498d199f2861582f
wo-o29 <woogur29@gmail.com>
2025-02-22 14:10:31 +0900

    feat: createElement 함수 추가(dom 생성)

---
+++ src/utils/dom.js
@@ -0,0 +1,22 @@
+export const createElement = (tag, props = {}) => {
+  if (!tag || typeof tag !== "string") {
+    throw new Error("태그는 문자열이어야 합니다.");
+  }
+  const element = document.createElement(tag);
+
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "style" && typeof value === "object") {
+      Object.entries(value).forEach(([styleKey, styleValue]) => {
+        element.style.setProperty(styleKey, styleValue);
+      });
+    }
+
+    if (key === "class") {
+      element.classList.add(value);
+    }
+
+    element[key] = value;
+  });
+
+  return element;
+};
